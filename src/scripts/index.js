a7e15ddb1386cccbfab7e3382911ed61a9969e14
raissaatha <raissaatha01@gmail.com>
2023-12-29 21:56:59 +0700

    restaurant app

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import 'regenerator-runtime'; /* for async await transpile */
+import '../styles/main.css';
+import '../styles/responsive.css';
+import 'boxicons';
+import '../../node_modules/boxicons/css/boxicons.min.css';
+import main from './script.js';
+
+document.addEventListener('DOMContentLoaded', main());
